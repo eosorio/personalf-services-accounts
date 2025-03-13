286897b9f279df64217d274f46a60dcf4c7f3ff4
Eduardo Osorio <eduardo.osorio.ti@gmail.com>
2025-03-13 08:11:10 +0000

    Replacing with podman

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@
 # Image will be tagged as devel. I need a pipeline to promote it to stable
 container_image_name=eosorio/personalf-services-accounts
 version_today=$(date "+%Y%m%d")
-alias container=podman
+container_bin=podman
 
 # Produced image should be dev and after testing promoted as stable (removing the dev tag)
-container build -t $container_image_name:${version_today}-dev -f personalf-services-accounts/Containerfile .
+$container_bin build -t $container_image_name:${version_today}-dev -f personalf-services-accounts/Containerfile .
 
 # Tagging image as latest
-container tag $container_image_name:${version_today}-dev $container_image_name:latest
+$container_bin tag $container_image_name:${version_today}-dev $container_image_name:latest
