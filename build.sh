ab7bb0a79ed7f09eff4d9e4ec4d2893b0d790c90
Eduardo Osorio <eduardo.osorio.ti@gmail.com>
2025-03-13 03:45:11 -0400

    Fix on Containerfile file name

--- build.sh
+++ build.sh
@@ -5,9 +5,10 @@
 # Image will be tagged as devel. I need a pipeline to promote it to stable
 container_image_name=eosorio/personalf-services-accounts
 version_today=$(date "+%Y%m%d")
+alias container=podman
 
 # Produced image should be dev and after testing promoted as stable (removing the dev tag)
-docker build -t $container_image_name:${version_today}-dev -f personalf-services-accounts/Dockerfile .
+container build -t $container_image_name:${version_today}-dev -f personalf-services-accounts/Containerfile .
 
 # Tagging image as latest
-docker tag $container_image_name:${version_today}-dev $container_image_name:latest
+container tag $container_image_name:${version_today}-dev $container_image_name:latest
